777d793c159d7f3c40f612a40b20fce80cc87fb4
Jesus Alberto Garcia Garcia <garciagg1090@gmail.com>
2024-11-25 11:36:54 -0600

    25/11/2024

--- stock_manager/src/models/clients.js
+++ stock_manager/src/models/clients.js
@@ -1,17 +1,17 @@
 const clientsQueries = {
-  // Obtener todos los clientes
+  // Se muestran todos los clientes
   getAll: `SELECT * FROM clients WHERE is_active = 1`,
 
-  // Obtener un cliente por su RFC
+  // Se muestran los clientes por su RFC
   getByRfc: `SELECT * FROM clients WHERE rfc = ?`,
 
-  // Obtener un cliente por su RFC o por su email (para validación al agregar o actualizar)
+  // Muestra un cliente por su RFC o por su email (Para agregar o actualizar)
   getByRfcOrEmail: `SELECT * FROM clients WHERE rfc = ? OR email = ?`,
 
-  // Crear un nuevo cliente
+  // Crea un nuevo cliente
   create: `INSERT INTO clients (rfc, first_name, last_name, birth_date, gender, phone_number, email, address) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
 
-  // Actualizar un cliente existente
+  // Actualiza un cliente existente dentro de la tabla
   update: `UPDATE clients SET first_name = ?, last_name = ?, birth_date = ?, gender = ?, phone_number = ?, email = ?, address = ? WHERE rfc = ?`,
 
   // Marcar un cliente como inactivo
--- stock_manager/src/models/products.js
+++ stock_manager/src/models/products.js
@@ -1,10 +1,10 @@
 const productsQueries = {
-  getAll: 'SELECT * FROM products', // Obtener todos los productos
+  getAll: 'SELECT * FROM products', // Enseñar todos los productos
   getWithStock: 'SELECT * FROM products WHERE stock > 0', // Obtener productos con stock mayor a 0
   getById: 'SELECT * FROM products WHERE id = ?', // Obtener producto por ID
-  create: 'INSERT INTO products (product, description, stock, measurement_unit, price, discount) VALUES (?, ?, ?, ?, ?, ?)', 
-  update: 'UPDATE products SET product = ?, description = ?, stock = ?, measurement_unit = ?, price = ?, discount = ? WHERE id = ?',
-  delete: 'UPDATE products SET stock = 0 WHERE id = ?', // Desactivar producto (en lugar de eliminar)
+  create: 'INSERT INTO products (product, description, stock, measurement_unit, price, discount) VALUES (?, ?, ?, ?, ?, ?)', //Creación del producto
+  update: 'UPDATE products SET product = ?, description = ?, stock = ?, measurement_unit = ?, price = ?, discount = ? WHERE id = ?', //Actualizar los productos
+  delete: 'UPDATE products SET stock = 0 WHERE id = ?', // Vaciar producto (en lugar de eliminar)
 };
 
 module.exports = { productsQueries };
