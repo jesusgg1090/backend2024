0c23d6401acf691d8ca8e2b768fa698dd8533c69
Jesus Alberto Garcia Garcia <garciagg1090@gmail.com>
2024-11-26 11:43:25 -0600

    Creacion de pokemon_game

---
+++ pokemon_game/src/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+class Server {
+    constructor(){
+        this.app = express();
+        this.port = 3000;
+
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares(){
+        this.app.use(express.json());
+    }
+    routes(){
+
+    }
+
+    start(){
+        this.app.listen(this.port, () => {
+            console.log(`Server is running on port ${this.port}`);
+
+        });
+    }
+}
+
+module.exports = Server;
